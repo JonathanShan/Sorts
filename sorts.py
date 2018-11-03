e912f3a0527bbf8ab2005756c3520baf36c65806
Jonathan Shan <johnny.shan@gmail.com>
2018-11-02 17:33:51 -0700

    push

--- sorts.py
+++ sorts.py
@@ -1,25 +1,51 @@
 import random
 import time
 
-def selection_sort(list):
-    pass
+def selection_sort(list_s):
+    comparison = 0
+    for i in range(len(list_s)-1):
+        minimum = list_s[i]
+        min_index = i
+        j = i + 1
+        while j in range(len(list_s)):
+            if list_s[j] < minimum:
+                minimum = list_s[j]
+                min_index = j
+            comparison += 1
+            j += 1
+        temp = list_s[i]
+        list_s[i] = minimum
+        list_s[min_index] = temp
+    return comparison
     
-def insertion_sort(list):
-    pass
+def insertion_sort(list_i):
+    comparison = 0
+    for static in range(1,len(list_i)):
+        moving = static
+        if list_i[moving] >= list_i[moving-1] and moving > 0:
+            comparison +=1
+        while list_i[moving] < list_i[moving-1] and moving > 0:
+            temp = list_i[moving]
+            list_i[moving] = list_i[moving-1]
+            list_i[moving-1] = temp
+            moving -= 1
+            comparison += 1
+    return comparison
+
    
 
-def main():
-    # Give the random number generator a seed, so the same sequence of 
-    # random numbers is generated at each run
-    random.seed(1234) 
+# def main():
+#     # Give the random number generator a seed, so the same sequence of 
+#     # random numbers is generated at each run
+#     random.seed(1234) 
     
-    # Generate 5000 random numbers from 0 to 999,999
-    randoms = random.sample(range(1000000), 5000)
-    start_time = time.time() 
-    comps = selection_sort(randoms)
-    stop_time = time.time()
-    print(comps, stop_time - start_time)
+#     # Generate 5000 random numbers from 0 to 999,999
+#     randoms = random.sample(range(1000000), 1000)
+#     start_time = time.time() 
+#     comps = insertion_sort(randoms)
+#     stop_time = time.time()
+#     print(comps, stop_time - start_time)
 
-if __name__ == '__main__': 
-    main()
+# if __name__ == '__main__': 
+#     main()
 
--- sorts_tests.py
+++ sorts_tests.py
@@ -9,5 +9,27 @@ class TestLab4(unittest.TestCase):
         self.assertEqual(comps, 1)
         self.assertEqual(nums, [10, 23])
 
+    def test_sort(self):
+    	nums = [1,2,3,4,5]
+    	comps = selection_sort(nums)
+    	self.assertEqual(comps, 10)
+    	self.assertEqual(nums, [1,2,3,4,5])
+
+    def test_sort1(self):
+    	nums = [1,2,3,4,5]
+    	comps = insertion_sort(nums)
+    	self.assertEqual(comps, 4)
+    	self.assertEqual(nums, [1,2,3,4,5])
+
+    def test_sort2(self):
+    	nums = [-4, 6, 2, 10, -3, -10]
+    	comps = insertion_sort(nums)
+    	self.assertEqual(nums, [-10, -4, -3, 2, 6, 10])
+
+    def test_sort3(self):
+    	nums = [3, 60, 1, -5, 2, 5, 6, 6]
+    	comps = selection_sort(nums)
+    	self.assertEqual(nums, [-5, 1, 2, 3, 5, 6, 6, 60])
+
 if __name__ == '__main__': 
     unittest.main()
